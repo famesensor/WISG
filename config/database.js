a3170494cb37dcab5d776728bc32d9367a7afd57
Sathit Junthong <44829080+famesensor@users.noreply.github.com>
2020-02-21 01:12:16 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 module.exports = {
-    database:'mongodb+srv://fame:'+encodeURIComponent('fame')+'@wdb-3q7ow.mongodb.net/test?retryWrites=true',
-    // database:'mongodb://localhost:27017/wish',
+    database:'mongodb://localhost:27017/wish',
     secret: 'yoursecret'
   }
   
